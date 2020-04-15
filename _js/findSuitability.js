72bd44251b328aad827039f131d732ddd73f6edd
Rens <renshuizer@freedom.nl>
2020-04-15 15:47:58 +0200

    Added maptool API functionality

--- _js/findSuitability.js
+++ _js/findSuitability.js
@@ -1,17 +1,13 @@
-//create latitude/longitude variables from the values of the input fields
-var lzLat = $("#lzLat").val();
-var lzLon = $("#lzLon").val();
-
-$(document).ready(function () {
-
-});
-
 //call check functions when submit button is clicked
 $("#suitSubmit").click(function () {
+
     //function that checks if the location is on water via onwater.io
     $.fn.checkIfWater();
     //function that checks if the wind speed isn't too high via weatherstack
     $.fn.checkWind();
+
+    //function that finds a sattelite image of the LZ via mapbox
+    $.fn.checkTerrain();
 
     //clear input fields
     $("#lzLat").val('');
@@ -20,6 +16,10 @@ $("#suitSubmit").click(function () {
 
 
 $.fn.checkIfWater = function () {
+    //create latitude/longitude variables from the values of the input fields
+    var lzLat = $("#lzLat").val();
+    var lzLon = $("#lzLon").val();
+
     //create the API Request URL with the values from the lat & long variables
     var onWaterRequest = 'https://api.onwater.io/api/v1/results/' + lzLat + ',' + lzLon + '?access_token=whz84VME51pQywVPCjBU';
 
@@ -34,8 +34,6 @@ $.fn.checkIfWater = function () {
 
     //fetch data from the API
     fetch(onWaterRequest).then(function (response) {
-        return response.json();
-    }).then(function (response) {
         //find the 'water' part of the object and check if it's true
         if (response.water == true) {
             //remove the loading icon when check is done
@@ -54,8 +52,13 @@ $.fn.checkIfWater = function () {
 };
 
 $.fn.checkWind = function () {
+    //create latitude/longitude variables from the values of the input fields
+    var lzLat = $("#lzLat").val();
+    var lzLon = $("#lzLon").val();
+
     //create the API Request URL with the values from the lat & long variables
     var wStackRequest = 'http://api.weatherstack.com/current?access_key=18f801e8fda64abe236ef016ddf9e0a9&query=' + lzLat + ',' + lzLon;
+
     //find the location for the loading icon
     var loadingIcon = $("#wind-loading");
     //find the location for the text
@@ -84,4 +87,26 @@ $.fn.checkWind = function () {
             suitText.html("\u2713 Wind speed okay");
         }
     })
+};
+
+$.fn.checkTerrain = function () {
+    //create latitude/longitude variables from the values of the input fields
+    var lzLat = $("#lzLat").val();
+    var lzLon = $("#lzLon").val();
+
+    //create the API Request URL with the values from the lat & long variables
+    var mapBoxRequest = 'https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/' + lzLat + ',' + lzLon + ',15,0/500x400?access_token=pk.eyJ1Ijoic3RhY2stc25lcnMiLCJhIjoiY2s4enk5emlhMDBpZjNpbnozbjhkYWUwbiJ9.e7lGfppTMSgOEHi4DRSIFg';
+
+    //find the location for the loading icon
+    var loadingIcon = $("#wind-loading");
+    //find the location for the text
+    var suitText = $("#wind");
+
+    //display loading icon when function starts
+    loadingIcon.addClass("loading-icon");
+    suitText.html("Rendering Sattelite Image");
+
+    //place sattelite image in #picture
+    $('#picture').attr("src", mapBoxRequest);
+    loadingIcon.removeClass("loading-icon");
 };
